886e8359db22c5beed8ab2e0af7aaf568c27a553
Fenwicked <fenwicked@gmail.com>
2018-11-05 08:04:38 -0700

    Create gameobject.java

---
+++ core/src/com/gtngame/game/gameobject.java
@@ -0,0 +1,14 @@
+public class gameobject {
+		public int sizeX;
+		public int sizeY;
+		public int positionX;
+		public int positionY;
+		public float motionX;
+		public float motionY;
+		public float yaw;
+  
+		public void gameobject(){}
+  
+		public void turn(){}
+		public void move(){}
+}
